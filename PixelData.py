b113c5a44726af04863d16cd1ba9db2d4aaa42ef
Ahilon <ziela111@gmail.com>
2020-01-22 20:36:45 +0100

    version 1.2

--- PixelData.py
+++ PixelData.py
@@ -3,53 +3,77 @@ import pyautogui
 from tkinter import *
 from PIL import ImageTk, Image
 
-# budowa okienka
-root = Tk() # tworzenie okna głównego
+# Create window
+root = Tk()
 root.title("Mouse Coordinate")
 root.iconbitmap("paint.ico")
 logoImage = ImageTk.PhotoImage(Image.open("logo.png"))
 
-def mouse():
+# Functions
+def mousePosition():
     x, y = pyautogui.position()
-    positionStr = 'X ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+    positionStr =  str(x).rjust(4) + ' X ' + str(y).rjust(4)
     return positionStr
 
-def colorPixel():
+def positionRGB():
     try:
         x, y = pyautogui.position()
         pixelColor = pyautogui.screenshot().getpixel((x, y))
-        color = ' RGB: (' + str(pixelColor[0]).rjust(3)
-        color += ', ' + str(pixelColor[1]).rjust(3)
-        color += ', ' + str(pixelColor[2]).rjust(3) + ')'
+        color = '(' + str(pixelColor[0]).rjust(3)
+        color += ',' + str(pixelColor[1]).rjust(3)
+        color += ',' + str(pixelColor[2]).rjust(3) + ')'
         return color
     except IndexError:
-        output =  ("poza skalą ")
+        output =  ("Out of scale")
         return output
 
-# set first label text
-mouseValue = StringVar(); mouseValue.set(mouse())
-colorPixelValue = StringVar(); colorPixelValue.set(colorPixel())
+def positionHex():
+    try:
+        x, y = pyautogui.position()
+        pixelColor = pyautogui.screenshot().getpixel((x, y))
+        hex = "#{0:02x}{1:02x}{2:02x}".format(pixelColor[0],pixelColor[1], pixelColor[2])
+        return hex
+    except IndexError:
+        output =  ("#F0F0F0")
+        return output
 
 
+# set values to function output
+mousePositionValue = StringVar(); mousePositionValue.set(mousePosition())
+positionRGBValue = StringVar(); positionRGBValue.set(positionRGB())
+positionHexValue = StringVar(); positionHexValue.set(positionHex())
 
+# define output labels
+mousePositionLabel = Label(root, width=30, textvariable=mousePositionValue, bd=2, relief=GROOVE)
+positionRGBLabel = Label(root, width=30, textvariable=positionRGBValue, bd=2, relief=GROOVE)
+positionHexLabel = Label(root, width=30,  textvariable=positionHexValue, bd=2, relief=GROOVE)
+positionColorLabel = Label(root, width=10, height= 5, bg=positionHex(), bd=2, relief=GROOVE)
+#define text labels
+resolutionTextLabel = Label(width=10, text='Resolution: ')
+RGBTextLabel = Label(width=10, text='RGB: ')
+hexTextLabel = Label(width=10, text='HEX: ')
+#define logo label
+logoLabel = Label(image=logoImage)
 
-# define labels
-mouseCoordinate = Label(root, width=40 , textvariable = mouseValue, bd=1, relief=SUNKEN)
-pixelColor = Label(root, width=40, textvariable = colorPixelValue, bd=1, relief=SUNKEN)
-logo = Label(image=logoImage)
-
-
-# Put labels on screen
-mouseCoordinate.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
-pixelColor.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
-
-logo.grid(row=3, column=0, columnspan = 3)
+# Put output labels on screen
+mousePositionLabel.grid(row=0, column=1, columnspan=3, padx=5, pady=5)
+positionRGBLabel.grid(row=1, column=1, columnspan=3, padx=5, pady=5)
+positionHexLabel.grid(row=2, column=1, columnspan=3, padx=5, pady=5)
+positionColorLabel.grid(row=3, column=0)
+# Put text labels
+resolutionTextLabel.grid(row=0, column=0)
+RGBTextLabel.grid(row=1, column=0)
+hexTextLabel.grid(row=2, column=0)
+# Put logo label on screen
+logoLabel.grid(row=3, column=1, columnspan=3)
 
 # dynamical label refresh
 while True:
     try:
-        mouseValue.set(mouse())
-        colorPixelValue.set(colorPixel())
+        mousePositionValue.set(mousePosition())
+        positionRGBValue.set(positionRGB())
+        positionHexValue.set(positionHex())
+        positionColorLabel.configure(background=positionHex())
         root.update()
     except TclError:
         sys.exit()
